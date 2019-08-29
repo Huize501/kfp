a2244cb983ee42ecfed77eb5d3e8b94f0f6e2d2c
Huize501 <erwinh@erwinh-macbookpro.roam.corp.google.com>
2019-08-29 16:51:08 +0200

    updated yaml for comp1

--- comp-from-scratch/kfp-test-component1/build_image.sh
+++ comp-from-scratch/kfp-test-component1/build_image.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-image_name=gcr.io/erwinh-mldemo/kfp-test # Specify the image name here
+image_name=gcr.io/erwinh-mldemo/kfp-test/component # Specify the image name here
 image_tag=latest
 full_image_name=${image_name}:${image_tag}
 base_image_tag=1.12.0-py3
@@ -8,5 +8,3 @@ cd "$(dirname "$0")"
 docker build --build-arg BASE_IMAGE_TAG=${base_image_tag} -t "${full_image_name}" .
 docker push "$full_image_name"
 
-# Output the strict image name (which contains the sha256 image digest)
-docker inspect --format="{{index .RepoDigests 0}}" "${IMAGE_NAME}"

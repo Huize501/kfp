d10c34f133b9dada2e49872c21a6842e9e42c3cc
Huize501 <erwinh@erwinh-macbookpro.roam.corp.google.com>
2020-01-06 13:56:08 +0800

    updated scikit component

--- scikit-component/test-run.sh
+++ scikit-component/test-run.sh
@@ -1,3 +1,3 @@
-python src/task.py --pathdata gs://erwinh-public-data/scikit/data/scikit-marketing-data.csv \
+python src/task.py --pathdata gs://erwinh-public-data/scikit/data/marketing-data.csv \
 --storage GCS --pathoutput gs://kfp-scikit/model/output/ \
 --pathoutputfile fileoutput 
